01b23e0a9daec4f44c8500b339bb4d6ccaf7de8a
minhee.son <44446425+Miniminis@users.noreply.github.com>
2019-05-16 10:37:30 +0900

    Change Array example 2

--- w03Heritage/src/boxEx2/InstanceOf.java
+++ w03Heritage/src/boxEx2/InstanceOf.java
@@ -1,6 +1,21 @@
 package boxEx2;
 
+/* 문제2.
+교안의 예제 InstanceOf.java를 instanceof 연산자를 사용하지 않는 형태로 
+변경하고자 한다. 즉 클래스의 상속관계를 그대로 유지하면서,
+ instanceof 연산자를 사용하지 않고도 동일한 실행결과를 보일 수 있어야 한다.
+ 변경되어야 할 wrapBox 메소드를 아래에 제시.
+
+Public static void wrapBox(Boxbox)
+{
+box.wrap();
+}
+
+HINT. 메소드 오버라이딩
+*/
+
 public class InstanceOf {
+	
 	public static void wrapBox(Box box) {
 		box.wrap();
 	}
@@ -10,26 +25,46 @@ public class InstanceOf {
 		PaperBox paperbox = new PaperBox();
 		GoldPaperBox goldpaperbox = new GoldPaperBox();
 		
-		wrapBox(box);
-		wrapBox(paperbox);
-		wrapBox(goldpaperbox);
+		Box box1 = box;
+		Box box2 = paperbox;
+		Box box3 = goldpaperbox;
+		
+		wrapBox(box1);
+		wrapBox(box2);
+		wrapBox(box3);
 	}
 	
 }
 
-class Box {	
-	public void wrap() {
+class Box {
+	
+	private void simpleWrap() {
 		System.out.println("simple wrap");
+	}
+	
+	public void wrap() {
+		simpleWrap();
 	}
 }
 class PaperBox extends Box {
-	public void wrap() {
+	
+	private void paperWrap() {
 		System.out.println("paper wrap");
+	}
+
+	public void wrap() {
+		paperWrap();
 	}
 	
 }
+
 class GoldPaperBox extends PaperBox{
+	
+	private void goldPaperBox() {
+		System.out.println("gold paper wrap");
+	}
+	
 	public void wrap() {
-		System.out.println("gold wrap");
+		goldPaperBox();
 	}
 }
